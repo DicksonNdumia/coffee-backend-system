828c74039ed4a33044c6ea91b85ee167425b2beb
DicksonNdumia <ndumiadickson19@gmail.com>
2026-04-19 11:17:00 +0300

    refactoring the upload of announcement images

--- src/routes/announcement.routes.js
+++ src/routes/announcement.routes.js
@@ -6,13 +6,14 @@ import {
     getAnnouncementById,
     getAnnouncements, updateAnnouncement
 } from "../controllers/announcement.controller.js";
+import upload from "../helper/uploads/upload.js";
 
 const router = express.Router();
 
-router.post('/', protect, isAdmin, createAnnouncement);
+router.post('/', protect, isAdmin,upload.single("image"), createAnnouncement);
 router.get('/', protect,getAnnouncements);
 router.get('/:id', protect, getAnnouncementById);
 router.delete('/:id', protect, isAdmin, deleteAnnouncement);
-router.put('/:id', protect, isAdmin, updateAnnouncement);
+router.put('/:id', protect, isAdmin,upload.single("image"), updateAnnouncement);
 
 export default router;
